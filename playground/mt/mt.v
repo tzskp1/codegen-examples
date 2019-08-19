0874c9fefb2e28de40ca5706597f897cbef5b3fb
tanakakensaku <tannakaken@gmail.com>
2019-08-19 12:27:42 +0900

    とりあえずsample/mt.rbと同じ挙動を取る函数を定義できた

--- playground/mt/mt.v
+++ playground/mt/mt.v
@@ -21,47 +21,80 @@ Definition upper_mask := 2147483648.
 Definition whole_mask := upper_mask * 2 - 1.
 Definition lower_mask := upper_mask - 1.
 
-Record random_state := {i : nat; x : seq N}.
+Record random_state := {index : nat; state_vector : seq N}.
 
-Fixpoint generate (rest : nat) (acc : seq N) : seq N :=
+Fixpoint generate_state_vector (rest : nat) (acc : seq N) : seq N :=
   match rest with
   | 0%nat => acc
   | 1%nat => acc
-  | S m => generate m ((N.land (1812433253 * (N.lxor (head 0 acc) (N.shiftr (head 0 acc) 30)) + N.of_nat(len - rest) + 1) whole_mask) :: acc)
+  | S m => generate_state_vector m ((N.land (1812433253 * (N.lxor (head 0 acc) (N.shiftr (head 0 acc) 30)) + N.of_nat(len - rest) + 1) whole_mask) :: acc)
   end.
 
-Definition initialize (seed : N) : random_state :=
+Definition initialize_random_state (seed : N) : random_state :=
   {|
-    i := 0;
-    x := rev (generate len  (N.land seed whole_mask :: nil));
+    index := 0;
+    state_vector := rev (generate_state_vector len  (N.land seed whole_mask :: nil));
   |}.
 
-Definition next (rand : random_state) : (N * random_state) :=
-  let data := x rand in
-  let ind := i rand in
+Definition next_random_state (rand : random_state) : (N * random_state) :=
+  let state_vec := state_vector rand in
+  let ind := index rand in
+  let current := nth 0 state_vec ind in
   let next_ind := Nat.modulo (ind +  1%nat) len in
+  let next := nth 0 state_vec next_ind in
   let far_ind := Nat.modulo (ind + m) len in
-  let z := N.lor (N.land (nth 0 data ind) upper_mask) (N.land (nth 0 data next_ind) lower_mask) in
-  let xi := N.lxor (N.lxor (nth 0 data far_ind) (N.shiftr z 1)) (if N.eqb (N.land z 1) 0 then 0 else a) in
+  let far := nth 0 state_vec far_ind in
+  let z := N.lor (N.land current upper_mask)
+                 (N.land next lower_mask) in
+  let xi := N.lxor (N.lxor far
+                           (N.shiftr z 1))
+                   (if N.eqb (N.land z 1) 0 then 0 else a) in
   let y1 := N.lxor xi (N.shiftr xi u) in
   let y2 := N.lxor y1 (N.land (N.shiftl y1 s) b) in
   let y3 := N.lxor y2 (N.land (N.shiftl y2 t) c) in
-  let y4 := N.lxor y3 (N.shiftl y3 l) in
+  let y4 := N.lxor y3 (N.shiftr y3 l) in
   let next_rand := {|
-        i := next_ind;
-        x := set_nth 0 data ind xi;
+        index := next_ind;
+        state_vector := set_nth 0 state_vec ind xi;
       |} in
   (y4, next_rand).
+
+Fixpoint nth_rand (n : nat) (rand : random_state) : N :=
+  let (r, next_state) := next_random_state rand in
+  match n with
+  | 0%nat => r
+  | S m => nth_rand m next_state
+  end.
+
+Definition nth_rand_with_seed (n : nat) (seed : N) : N :=
+  let rand := initialize_random_state seed in
+  nth_rand n rand.
+
+Compute initialize_random_state 20150919.
+
+Compute nth_rand_with_seed 0 20150919.
+Compute nth_rand_with_seed 1 20150919.
+Compute nth_rand_with_seed 2 20150919.
 
 CodeGen Terminate Monomorphization N.land.
 CodeGen Terminate Monomorphization N.lor.
 CodeGen Terminate Monomorphization N.lxor.
 CodeGen Terminate Monomorphization N.shiftl.
 CodeGen Terminate Monomorphization N.shiftr.
-CodeGen Monomorphization initialize.
-CodeGen Monomorphization next.
-Print _initialize.
-Print _generate.
-Print _next.
+CodeGen Monomorphization initialize_random_state.
+CodeGen Monomorphization next_random_state.
+CodeGen Monomorphization nth_rand.
+CodeGen Monomorphization nth_rand_with_seed.
+Print _initialize_random_state.
+Print _generate_state_vector.
+Print _next_random_state.
+Print _nth_rand.
+Print _nth_rand_with_seed.
 
-CodeGen GenCFile "mt_generated.c" _generate _initialize.
+CodeGen GenCFile "mt_generated.c"
+        _generate_state_vector
+        _initialize_random_state
+        _next_random_state
+        _nth_rand
+        _nth_rand_with_seed.
+        
