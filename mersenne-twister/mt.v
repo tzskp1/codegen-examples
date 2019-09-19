3ee2ee5177d4896fe07fba062554a64bed3f4607
Takafumi Saikawa <tscompor@gmail.com>
2019-09-19 17:45:00 +0900

    declare mathcomp-standard settings for implicit arguments; add .gitignore and remove temporary files

--- mersenne-twister/mt.v
+++ mersenne-twister/mt.v
@@ -1,6 +1,10 @@
 (* Implementation of MT19937 *)
 From mathcomp Require Import all_ssreflect.
 
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
 Require Import BinNat.
 
 Require Import codegen.codegen.
