8ea212fdb49f540abdb4418c27e86a9633a48e09
Tanaka Kazunari <tzskp1@gmail.com>
2019-11-20 23:34:33 +0900

    cycle.v -> irreducible.v

--- mersenne-twister/irreducible.v
+++ mersenne-twister/irreducible.v
@@ -633,39 +633,3 @@ apply/(iffP idP).
   by rewrite !modp_small ?GRing.mulr1 ?size_polyC ?eqxx.
 Qed.
 End irreduciblity.
-  
-From codegen Require Import codegen.
-Require Import mt.
-
-From infotheo Require Import f2 ssralg_ext.
-Require Import BinNat.
-
-Set Implicit Arguments.
-Unset Strict Implicit.
-Unset Printing Implicit Defensive.
-
-Open Scope N_scope.
-
-Definition mt19937_cycle : nat := 2 ^ 19937 - 1.
-
-Require mt_alg.
-
-Fail Lemma mt_alg_eq_mt : forall seed n,
-    mt_alg.nth_random_value seed n = nth_random_value seed n.
-
-Definition cyclic (f : nat -> N) cycle := forall n, f n = f (n + cycle)%nat.
-
-Fail Lemma Mersenne_Twister_Cycle_alg n seed :
-  cyclic (mt_alg.nth_random_value seed).
-
-Section mt19937_cyclic.
-Variable seed : N.
-
-Lemma Mersenne_Twister_Cycle :
-  cyclic (nth_random_value seed) mt19937_cycle.
-Abort.
-
-Lemma least_cycle cycle :
-  (cycle < mt19937_cycle)%nat -> ~ cyclic (nth_random_value seed) cycle.
-
-End mt19937_cyclic.
