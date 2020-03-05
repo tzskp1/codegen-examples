5254b1c9263e904519b588f2a97412b85d3f015c
Kazunari Tanaka <tzskp1@gmail.com>
2020-03-05 13:00:50 +0900

    build the statement of cyclicity

--- mersenne-twister/cycle.v
+++ mersenne-twister/cycle.v
@@ -122,52 +122,14 @@ apply/(iffP idP).
 * by case/irreducible.irreducibleP/andP.
 Qed.
 
-(* Lemma cycleB_dvdP q : reflect (horner_mx (castmx (tecp, tecp) B) ('X ^+ q) == 1) *)
-(*                               (2 ^ p - 1 %| q)%nat. *)
-(* Proof. *)
-
-
-Check B *m B.
-Check powers_mx.
-Variable q : nat.
-
-
-End phi.
-Definition w := 32.
-Definition r := 1.
-Definition n := 4.
-Definition m := 1.
-
-Lemma mn : m < n.
-  done.
-  Qed.
-Lemma rw : r < w.
-  done.
-  Qed.
-
-Compute incomplete_array mn rw 0.
-Lemma sizea : size ([:: 1;0;0;1;1;0;0;1;0;0;0;0;1;0;0;0;1;0;1;1;0;0;0;0;1;1;0;1;1;1;1;1] : seq 'F_2)%R == w.
-  done.
-Qed.
-
-Lemma nm : m < n.
-  by [].
-Defined.
-Lemma pm'': prime (2 ^ (n * w - r) - 1).
+Lemma cycleB : irreducible_poly phi ->
+  (horner_mx (castmx (tecp, tecp) B) ('X ^+ (2 ^ (size phi).-1 - 1)) == 1).
 Proof.
-  Admitted.
-Definition B := lin1_mx (@f _ _ _ _ (Tuple sizea) pm'' nm erefl erefl erefl).
-Compute B.
-Variable j : 'I_(size (phi n m r (Tuple sizea))).-1.
-Check lin_mx.
-Check lin1_mx.
-Lemma BE : B (delta_mx 0%R j) = B (delta_mx 0%R j) .
-rewrite /B /f.
-
-rewrite /= /rVnpoly /comp rVpoly_delta.
-rewrite /irreducible.mulX /irreducible.mulV.
-rewrite /insubd.
-      Check row_mx 0.
-Check delta_mx
-
-Compute B 0.
+move=> H0; move/irreducibleP: (H0) => H1.
+have H2: phi = mxminpoly (castmx (tecp, tecp) B).
+ move: (mxminpoly_dvd_char (castmx (tecp, tecp) B)).
+ rewrite /phi.
+ Check horner_mxK (castmx (tecp, tecp) B) ('X^(2 ^ (size phi).-1 - 1)).
+ Check irreducible.X2mp_eq1 pm' H0.
+ Admitted.
+End phi.
