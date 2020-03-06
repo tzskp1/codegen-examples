aaa6b6b080da00ff580c1804012d9be42a0a385b
Kazunari Tanaka <tzskp1@gmail.com>
2020-03-06 16:51:50 +0900

    more and more

--- mersenne-twister/gluing.v
+++ mersenne-twister/gluing.v
@@ -13,6 +13,10 @@ Definition r := 31.
 Definition n := 624.
 Definition m := 397.
 
+Inductive random_state_aux (y : random_state) :=
+| Prf : (index y < len)%nat && (size (state_vector y) == len)
+        -> random_state_aux y.
+
 Definition N_of_word (t : w.-tuple 'F_2) :=
   foldr (fun x y => 2*y + x) 0
         (map_tuple (fun x => if (x == 1 :> 'F_2)%R then 1%N else 0) t).
@@ -66,17 +70,20 @@ Definition state_of_array (y : 'rV['F_2]_(n * w - r)) :=
       (mktuple (fun j => mktuple (@array_incomplete w n m r erefl erefl y j)));|}.
 
 Definition array_of_state (y : random_state) :=
-  map word_of_N (rot (index y) (state_vector y)).
-Check @incomplete_array w n m r erefl erefl.
-Check array_of_state.
-   'rV['F_2]_(n * w - r).
-  {| index:=0;
-     state_vector:=map N_of_word
-      (mktuple (fun j => mktuple (@array_incomplete w n m r erefl erefl y j)));|}.
+  @incomplete_array w n m r erefl erefl
+  (\matrix_(i, j) nth 0%R (nth (word_of_N 0)
+                  [seq word_of_N i | i <- rot (index y) (state_vector y)] i) j).
 
-(* Compute rot 2 (iota 0 4). *)
-
-
-
-(* Definition array_incomplete (y : 'rV['F_2]_p) *)
-(* Compute rot 2 (iota 0 4). *)
+Lemma state_of_arrayK : cancel state_of_array array_of_state.
+Proof.
+  move=> x.
+  rewrite /array_of_state /state_of_array rot0 -map_comp.
+  set T := (\matrix_(_, _) _)%R.
+  have->: T = (@array_incomplete w n m r erefl erefl x)%R.
+   apply/matrixP => i j.
+   subst T.
+   rewrite !mxE (nth_map (word_of_N 0)) /=.
+    by rewrite N_of_wordK !nth_mktuple ?(castmxE, mxE).
+   by rewrite size_tuple card_ord.
+  by rewrite array_incompleteK.
+Qed.
