f777a897171b30b01f278c6b4ad0f0867a561c6b
Kazunari Tanaka <tzskp1@gmail.com>
2020-06-25 20:39:41 +0900

    hew out

---
+++ mersenne-twister/primitivity.v
@@ -0,0 +1,160 @@
+From mathcomp Require Import all_ssreflect all_algebra all_field all_fingroup.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+From Coqprime Require Import PocklingtonRefl.
+
+Lemma ZposE p : Z.pos p = Z.of_nat (Pos.to_nat p).
+Proof.
+  elim: p => // p H.
+  + by rewrite Pos2Nat.inj_xI Nat2Z.inj_succ Nat2Z.inj_mul /= -H.
+  + by rewrite Pos2Nat.inj_xO Nat2Z.inj_mul /= -H.
+Qed.
+
+Lemma NposE p : N.pos p = Pos.to_nat p :> nat.
+Proof.
+  elim: p => // p H.
+  + by rewrite Pos2Nat.inj_xI /= natTrecE Nat.add_0_r -addnn -plusE -!H.
+  + by rewrite Pos2Nat.inj_xO /= natTrecE Nat.add_0_r -addnn -plusE -!H.
+Qed.
+
+Local Lemma trivial x0 : Z.of_nat x0.+1 = Z.of_nat x0 + 1.
+Proof.
+  rewrite /Z.of_nat.
+  case: x0 => // x0.
+  by rewrite /= Pos2Z.inj_add /= Pos.add_1_r.
+Qed.
+
+Lemma of_nat_hom' x0 x : Z.of_nat (x0 + x) = Z.of_nat x0 + Z.of_nat x.
+Proof.
+  elim: x0 x => // x0 IHx0 x.
+  by rewrite trivial addSn -addnS IHx0 -[in RHS]Z.add_assoc [X in _ = _ + X]Z.add_comm -trivial.
+Qed.
+
+Lemma of_nat_hom x0 x : Z.of_nat (x0 * x) = Z.of_nat x0 * Z.of_nat x.
+Proof.
+  elim: x0 x => // x0 IHx0 x.
+  by rewrite trivial Z.mul_add_distr_r -IHx0 mulSn addnC of_nat_hom' Z.mul_1_l.
+Qed.
+
+Lemma of_nat_inj x0 x : Z.of_nat x0 = Z.of_nat x -> x0 = x.
+Proof.
+  elim: x0 x => [[]//|] x0 IHx0 []// x.
+  by rewrite !trivial !Z.add_1_r => /Z.succ_inj/IHx0 ->.
+Qed.
+
+Lemma dvdnP' x m : reflect (Z.of_nat x | Z.of_nat m) (dvdn x m).
+Proof.
+  apply/(iffP idP) => H.
+  + case/dvdnP: H => x0 p.
+    exists (Z.of_nat x0); by rewrite -of_nat_hom p.
+  + case: H => x0 p.
+    apply/dvdnP.
+    case: x0 p.
+    * rewrite Z.mul_0_l.
+      case: m => // _.
+      by exists 0%nat; rewrite mul0n.
+    * move=> x0 p.
+      exists (N.pos x0).
+      rewrite ZposE -of_nat_hom in p.
+      by rewrite NposE (of_nat_inj p).
+    * case: x.
+      - case: m => // *.
+        by exists 0%nat; rewrite muln0.
+      - move=> x p /(f_equal Z.sgn).
+        by case: m.
+Qed.
+
+Lemma to_nat_pos m : (0 < Pos.to_nat m)%nat.
+Proof.
+  elim: m => // m IHm.
+  apply (leq_trans IHm) => {IHm}.
+  by rewrite -!NposE /= natTrecE -addnn leq_addr.
+Qed.
+
+Lemma of_nat_lt m n : reflect (Z.of_nat m < Z.of_nat n) (m < n)%nat.
+Proof.
+  apply/(iffP idP) => H.
+  + elim: m n H => [[]//|] m IH []// n.
+    rewrite ltnS => {}/IH H.
+    rewrite -addn1 -[X in _ < _ X]addn1 !of_nat_hom' /= !Z.add_1_r.
+    case: (Z.succ_lt_mono (Z.of_nat m) (Z.of_nat n)) => H0 _.
+    apply/H0/H.
+  + elim: m n H => [[]//|] m IH []// n.
+    rewrite !ltnS => H.
+    apply/IH; move: H.
+    rewrite -addn1 -[X in _ < _ X]addn1 !of_nat_hom' /= !Z.add_1_r.
+    case: (Z.succ_lt_mono (Z.of_nat m) (Z.of_nat n)) => _ H0.
+    apply/H0.
+Qed.
+
+Lemma primeP' n : reflect (prime (Z.of_nat n)) (prime.prime n).
+Proof.
+  apply/(iffP idP) => H.
+  + constructor.
+    - case: n H => []//[]// n _.
+      apply Pos.lt_1_succ.
+    - move=> m H1mn.
+      constructor.
+      * exists m; by rewrite Z.mul_1_r.
+      * exists (Z.of_nat n); by rewrite Z.mul_1_r.
+      * case => [[]// ?|x |x ].
+        + rewrite Z.mul_0_r => m0.
+          by move: m0 H1mn => -> [].
+        + case: m H1mn => [[]//|m H1mn|?[]//].
+          rewrite !ZposE => /dvdnP' H1 /dvdnP' H2.
+          apply/dvdnP'; rewrite dvdn1.
+          case/primeP: H => n1 H.
+          case/orP: (H _ H2) => // /eqP xn.
+          move: xn H1 => -> /dvdn_leq H0.
+          case: H1mn => m1 mn.
+          have/H0: (0 < Pos.to_nat m)%nat by apply to_nat_pos.
+          have: (Pos.to_nat m < n)%nat.
+           rewrite /= ZposE in mn.
+           by move/of_nat_lt : mn.
+          by rewrite ltnNge => /negP H3 {}/H3.
+        + case: m H1mn => [[]//|m H1mn|?[]//].
+          rewrite -!Z.divide_Zpos_Zneg_l !ZposE => /dvdnP' H1 /dvdnP' H2.
+          apply/dvdnP'; rewrite dvdn1.
+          case/primeP: H => n1 H.
+          case/orP: (H _ H2) => // /eqP xn.
+          move: xn H1 => -> /dvdn_leq H0.
+          case: H1mn => m1 mn.
+          have/H0: (0 < Pos.to_nat m)%nat by apply to_nat_pos.
+          have: (Pos.to_nat m < n)%nat.
+           rewrite /= ZposE in mn.
+           by move/of_nat_lt : mn.
+          by rewrite ltnNge => /negP H3 {}/H3.
+  + apply/primeP; split.
+    - case: H => H1 H2.
+      by apply/of_nat_lt/H1.
+    - move=> d dn.
+      case: H => H1 H2.
+      case d1: (d == 1)%nat => //.
+      case: d d1 dn => [_|[]// d _ dn].
+      * by rewrite dvd0n eq_sym.
+        rewrite /rel_prime in H2.
+      * case d2: (d.+2 == n)%nat => //.
+        have/H2 :1 <= Z.of_nat d.+2 < Z.of_nat n.
+         split.
+         - rewrite -addn1 !of_nat_hom'.
+           have->: 1 = 0 + 1 by rewrite Z.add_0_l.
+           apply Zplus_le_compat_r, Zle_0_nat.
+         - have/dvdn_leq: (0 < n)%nat by case: n H1 {dn H2 d2}.
+           by move=> /(_ _ dn); rewrite leq_eqVlt d2 => /of_nat_lt.
+        case => _ _ /(_ (Z.of_nat d.+2)) H.
+        have/dvdn_leq: (d.+2 %| 1)%nat by apply/dvdnP'/H; apply/dvdnP'.
+        rewrite ltnS leqnn ltnS ltn0.
+        by apply.
+Qed.
+
+Lemma pm : prime.prime (2 ^ 19937 - 1)%nat.
+Proof.
+  Admitted.
+(*
+  apply/primeP'.
+  suff: (LucasLehmer.lucas_test 19937).
+   suff->: (Z.of_nat (2 ^ (n * w - r) - 1)) = 2 ^ 19937 - 1 by apply LucasLehmer.LucasTest.
+*)
